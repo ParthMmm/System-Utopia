b53b8961edf9344ff5f3425179ec1236aeb31c67
Parth Mangrola <parthmangrola@gmail.com>
2019-03-13 16:01:14 -0700

    done

--- ucr/piano.h
+++ ucr/piano.h
@@ -5,17 +5,13 @@
 
 
 #define B3  247
-
 #define CS4 277
 
-#define DS4 311
-
+#define DS4 155
 #define FS4 370
 #define G4  392
-
 #define A4  440
 #define AS4 466
-
 #define D5  587
 #define DS5 622
 
